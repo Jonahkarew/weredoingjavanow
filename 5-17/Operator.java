0a100cc2513ccebc9499835c5e6a92ecc7196fb8
Jonah Karew <jonahkarew@gmail.com>
2021-05-17 15:59:04 -0400

    operators

---
+++ 5-17/Operator.java
@@ -0,0 +1,28 @@
+public class Operator {
+    public static void main(String[] args){
+        int a = 28;
+        int b = 7;
+        int c = 10;
+
+        // addition
+        System.out.println(a + b);
+        // subtraction
+        System.out.println(a - b);
+        // multiplcation
+        System.out.println(a * b);
+        // division
+        System.out.println(a / b);
+        // remainder
+        System.out.println(c % b);
+        // increment
+        System.out.println(++a);
+
+
+        // not an operator, just a reset
+        a = 28;
+        // decrement
+
+        
+        System.out.println(--a);
+    }
+}
