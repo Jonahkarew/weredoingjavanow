ea78bfd36b773b3f59e1f7c01cb46afd57625fa9
Jonah Karew <jonahkarew@gmail.com>
2021-05-20 17:37:59 -0400

    while loops

---
+++ 520/WhileLoop.java
@@ -0,0 +1,11 @@
+public class WhileLoop {
+    public static void main(String[] args){
+        // while loop does not have to execute even once
+        int i = 0;
+        while (i < 10){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Loop is over");
+    }
+}
