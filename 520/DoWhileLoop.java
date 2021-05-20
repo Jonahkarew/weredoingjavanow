d841a91e3fc2ab05d02aec19e2aa61f442e3031e
Jonah Karew <jonahkarew@gmail.com>
2021-05-20 17:44:07 -0400

    do while loops

---
+++ 520/DoWhileLoop.java
@@ -0,0 +1,13 @@
+public class DoWhileLoop {
+    public static void main(String[] args){
+        int i = 110;
+
+        // do while loops have to execute at least once
+
+        do {
+            System.out.println("i is equal to " + i);
+            i++;
+        }
+        while( i <10) ;
+    }
+}
